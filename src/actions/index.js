d5b718c043b3ddbb62486d315a70db1ee54be73a
Casey The Great <casey@caseypitman.com>
2020-11-25 11:16:15 -0600

    Created PostList component. Created fetchPosts action. Created axios instance for jsonPlaceholder api.

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// Action Creators
+
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+
+   const response = await jsonPlaceholder.get('./posts')
+
+
+
+  return {
+    type: "FETCH_POSTS",
+    payload: response
+  };
+};
