9c737710876c6ec552d262a5b2a84014f4a8d848
Casey The Great <casey@caseypitman.com>
2020-11-29 09:39:42 -0600

    Memoized FETCH_USER.

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,9 +9,11 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = id => {
-  return async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: "FETCH_USER", payload: response.data });
-  };
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
